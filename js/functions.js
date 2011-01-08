fa6fd1f8e7373c1f429d542474db095d01458b49
Jair Henrique <jair.henrique@gmail.com>
2011-01-07 22:44:51 -0200

    Melhorias no visual. Atualização do jQuery. Organização dos arquivos.

---
+++ js/functions.js
@@ -0,0 +1,49 @@
+window.addEventListener("load", windowLoaded, false);
+function windowLoaded() {
+	chrome.tabs.getSelected(null, function(tab) {
+		apontame(tab.url);
+	});
+}
+
+apontame = function(url) {
+	$.ajax({
+		timeout: 10000,
+		url: "http://www.aponta.me/add",
+		data: ({'wt': 'json', 'url': url}),
+		dataType: 'json',
+		type: "GET",
+		success: function(data) {
+			if(data.shortURL == 'null'){
+				showShortUrl('Ops! Falhei =/', false);
+			} else {
+				showShortUrl(data.shortURL, true);
+				generateQR(data.shortURL);
+			}
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown){
+			showShortUrl('Ops! Falhei =/', false);
+		}
+	});
+};
+
+generateQR = function(url){
+	$("#qrCode img").attr('src', "http://chart.apis.google.com/chart?chs=200x200&cht=qr&chl="+url).fadeIn();
+};
+
+showShortUrl = function(url, hasShortUrl) {
+	if ( hasShortUrl ) {
+		var htmlUrl = "";
+		htmlUrl += '<input type="text" id="txtUrl" value="'+url+'">';
+		htmlUrl += '<br />';
+		htmlUrl += '<input type="button" id="copyUrl" value="Copiar">';
+					
+		$("#apontame").html(htmlUrl);
+		$("#copyUrl").click(function(){
+			$("#txtUrl").select(); 
+			document.execCommand("copy", false, null);
+			$(this).val("Link copiado!");
+		});
+	} else {
+		$("#apontame").html(url);
+	}
+};
